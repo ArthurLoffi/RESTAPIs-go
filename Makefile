d04293d74a515fb72ac522f0e0100344e1c78382
ArthurLoffi <arthurloffisoares@outlook.com>
2026-05-21 23:03:44 -0300

    feat: changed delete-user to neon tech postgresql with gorm
    Files were also changed for the Linux system.

---
+++ Makefile
@@ -0,0 +1,13 @@
+build:
+	go mod tidy
+	go build ./cmd/api/.
+
+run:
+	./api
+
+docs:
+	swag init -g ./cmd/api/main.go --output ./docs
+
+# Ao instalar o repo, rodar este comando
+init:
+	go mod download
--- internal/use-cases/delete-user.go
+++ internal/use-cases/delete-user.go
@@ -2,8 +2,8 @@ package use_cases
 
 import (
 	"net/http"
+	db "restapis-go/internal/repository"
 	user "restapis-go/internal/entities"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,24 +14,23 @@ import (
 // @Tags         users
 // @Produce      json
 // @Success      200  {array}  user.User
-// @Router       /users/delete [delete]
+// @Router       /users/delete/:ID [delete]
 func DeleteUser(c *gin.Context) {
 	idString := c.Param("ID")
 
-	id, err := strconv.ParseUint(idString, 10, 64)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Invalid ID",
+	var user user.User
+	result := db.Database.First(&user, idString)
+
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "user not found",
 		})
+		return
 	}
 
-	for i, u := range user.Users {
-		if u.ID == uint(id) {
-			user.Users = append(user.Users[:i], user.Users[i+1:]...)
-			c.JSON(http.StatusOK, gin.H{"message": "Deleted user sucessfully"})
-			return
-		}
-	}
+	db.Database.Delete(&user)
 
-	c.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+	c.JSON(http.StatusOK, gin.H{
+		"message": "user deleted successfully!",
+	})
 }
